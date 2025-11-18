2123363a30dfaf8d6186ea4334b08625c1592c7a
yo252yo <yo252yo@gmail.com>
2025-11-18 19:27:56 +0100

    clarify the dependency to make index.template.html more generic and clear

--- python/website/website.py
+++ python/website/website.py
@@ -6,12 +6,26 @@ from jinja2 import Template
 
 from python.common import LANGUAGES, print_confirmation
 
-BASE_PATH = "books/philosophy_friends"
 
+def list_books(path: str) -> List[Tuple[str, str]]:
+    books = []
+    for book in os.listdir(path):
+        book_path = os.path.join(path, book)
+        if not os.path.isdir(book_path) or not os.path.isdir(os.path.join(book_path, "INPUT")):
+            continue
+        for lang in LANGUAGES:
+            target_dir = os.path.join(book_path, lang)
+            if os.path.isdir(target_dir) and os.path.exists(os.path.join(target_dir, "thumbnail.png")):
+                books.append((book, lang))
+    return sorted(books)
+
+JINJA_VARIABLES = {
+    "philsoophy_friends_books": list_books("books/philosophy_friends")
+}
 
 def copy_reader_template() -> None:
-    for book in os.listdir(BASE_PATH):
-        book_path = os.path.join(BASE_PATH, book)
+    for book in os.listdir("books/philosophy_friends"):
+        book_path = os.path.join("books/philosophy_friends", book)
         if not os.path.isdir(os.path.join(book_path, "INPUT")):
             continue
         for lang in LANGUAGES:
@@ -20,7 +34,18 @@ def copy_reader_template() -> None:
                 shutil.copy("books/reader.template.html", os.path.join(target_dir, "reader.html"))
                 print_confirmation(os.path.join(target_dir, "reader.html"))
 
-def process_html_templates(books_list: List[Tuple[str, str]]):
+def handle_template(root, file):
+    template_path = os.path.join(root, file)
+    output_path = template_path.replace(".template.html", ".html")
+    with open(template_path, "r", encoding="utf-8") as f:
+        template_content = f.read()
+    template = Template(template_content)
+    html_content = template.render(var=JINJA_VARIABLES)
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(html_content)
+    print_confirmation(output_path)
+
+def process_html_templates():
     for root, _, files in os.walk("books"):
         for file in files:
             if file.endswith(".template.html"):
@@ -28,28 +53,8 @@ def process_html_templates(books_list: List[Tuple[str, str]]):
                     copy_reader_template()
                     continue # We copy this template, we don't compile it.
 
-                template_path = os.path.join(root, file)
-                output_path = template_path.replace(".template.html", ".html")
-                with open(template_path, "r", encoding="utf-8") as f:
-                    template_content = f.read()
-                template = Template(template_content)
-                html_content = template.render(books=books_list)
-                with open(output_path, "w", encoding="utf-8") as f:
-                    f.write(html_content)
-                print_confirmation(output_path)
+                handle_template(root, file)
 
-def get_books() -> List[Tuple[str, str]]:
-    books = []
-    for book in os.listdir(BASE_PATH):
-        book_path = os.path.join(BASE_PATH, book)
-        if not os.path.isdir(book_path) or not os.path.isdir(os.path.join(book_path, "INPUT")):
-            continue
-        for lang in LANGUAGES:
-            target_dir = os.path.join(book_path, lang)
-            if os.path.isdir(target_dir) and os.path.exists(os.path.join(target_dir, "thumbnail.png")):
-                books.append((book, lang))
-    return books
 
 def make_website() -> None:
-    books = get_books()
-    process_html_templates(sorted(books))
+    process_html_templates()
